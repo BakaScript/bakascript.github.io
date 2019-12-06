7b7bf3228db2b6335c1ccd1ae2634cbeda6a148a
Fajar <fajars347@yahoo.com>
2019-12-06 07:36:53 +0700

    add back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,22 @@ AOS.init({
     once : true
 });
 
-$(document).ready(function(){
-    $(document).scroll(function(){
-        console.log('ok') ;
-    }) ;
-}) ;
 
 function whatstappMe() {
-    window.open('https://api.whatsapp.com/send?phone=+6282216538309&text=boleh tanya soal mobilnya?', '_blank') ;
+    window.open('https://api.whatsapp.com/send?phone=+6282216538309&text=Halo Pak Arif, saya liat dari web mohon info promo Suzuki nya', '_blank') ;
 }
+
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll-top').css('opacity', '1') ; 
+        } else { 
+            $('#scroll-top').css('opacity', '0'); 
+        } 
+    }); 
+    $('#scroll-top').click(function(){ 
+        $("html, body").animate({ scrollTop: target.offset().top }, 1000); 
+        return false; 
+    }); 
+});
